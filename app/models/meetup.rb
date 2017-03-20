630efcd24ed1db16b22e774670ae4b2d945254d8
niclin <bboyceo@hotmail.com>
2017-03-20 23:36:14 +0800

    Add comment model

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,2 +1,3 @@
 class Meetup < ApplicationRecord
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320150648) do
+ActiveRecord::Schema.define(version: 20170320153241) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "meetup_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meetups", force: :cascade do |t|
     t.string   "title"
