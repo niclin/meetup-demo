c9ec81297151fbeafa4473dda3858500bd2c649d
niclin <bboyceo@hotmail.com>
2017-03-22 12:13:54 +0800

    User can update data from api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::UsersController < Api::V1::BaseController
+
+  before_action :authenticate_user!, only: [:update]
+
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(update_params)
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name)
+  end
+
 end
